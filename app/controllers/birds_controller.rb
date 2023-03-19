115962e1dfa9a9bfccd821e967a8854d2b9041c2
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-03-19 15:08:29 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -11,6 +11,16 @@ class BirdsController < ApplicationController
     bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
+  # create increment_likes
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
 
   # GET /birds/:id
   def show
@@ -25,7 +35,17 @@ class BirdsController < ApplicationController
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
+  end
+  #update
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
   end
 
 end
